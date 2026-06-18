fe4bd322ad276721a2f00036ce02e1811d7a85fd
Omar Abumuhfouz <omarabumuhfouz@gmail.com>
2026-06-18 10:30:53 +0300

    add all changes

--- PlayWithGithupActions/User.cs
+++ PlayWithGithupActions/User.cs
@@ -1,6 +1,8 @@
 namespace PlayWithGithupActions;
+
 public class User
 {
     public Guid Id { get; set; }
     public string Name { get; set; } = string.Empty;
+    public string NewVersion { get; set; } = string.Empty;
 }
